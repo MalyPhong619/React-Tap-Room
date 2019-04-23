dbfe653f96912ef161f22aa098b1a13044bd97fc
Maly Phongsavanh <phongsavanh619@icloud.com>
2019-04-22 18:15:52 -0700

    add all taps from angular to react

--- src/components/TapList.jsx
+++ src/components/TapList.jsx
@@ -38,7 +38,108 @@ var masterTapList = [
     type: 'English Barley Wine',
     price: '6'
   },
-
+  {
+    brand: 'Moonraker',
+    name: 'Foam Walker',
+    type: 'Stout',
+    price: '9'
+  },
+  {
+    brand: 'Gigantic',
+    name: 'Corpse Riviver',
+    type: 'Sour Ale',
+    price: '7'
+  },
+  {
+    brand: 'Block 15',
+    name: 'Wonka\'s Wit',
+    type: 'Wild Witbier',
+    price: '9'
+  },
+  {
+    brand: 'Little Beast',
+    name: 'Wold Camp',
+    type: 'Hoppy Saison',
+    price: '7'
+  },
+  {
+    brand: 'Ex Novo',
+    name: 'Antwerp Delight',
+    type: 'Belgian Pale Ale',
+    price: '6'
+  },
+  {
+    brand: 'Way Finder',
+    name: 'Hausbier: Beez In The Trap',
+    type: 'English SUmmer Ale',
+    price: '6'
+  },
+  {
+    brand: 'Pfriem',
+    name: 'Dark Czech Lager',
+    type: 'Czech Dark Lager',
+    price: '6'
+  },
+  {
+    brand: 'Pizza Port',
+    name: 'ChronicAle',
+    type: 'American Amber Ale',
+    price: '5'
+  },
+  {
+    brand: 'Manifest',
+    name: 'Best Lager',
+    type: 'Light Lager',
+    price: '5'
+  },
+  {
+    brand: 'Pelican',
+    name: 'Pilsner',
+    type: 'German Pilsner',
+    price: '6'
+  },
+  {
+    brand: 'Logsdon Farmhouse',
+    name: 'Weisse City',
+    type: 'Hefeweizen',
+    price: '6'
+  },
+  {
+    brand: 'Bend',
+    name: 'Devil\'s Cabbage',
+    type: 'West Coast IPA',
+    price: '6'
+  },
+  {
+    brand: 'El Segundo',
+    name: 'Dreamin\' of Sunshine',
+    type: 'IPA',
+    price: '7'
+  },
+  {
+    brand: 'Modern Times',
+    name: 'Dfreamspeaker',
+    type: 'Hazy IPA',
+    price: '7'
+  },
+  {
+    brand: 'Georgetown',
+    name: 'Meowsa',
+    type: 'Imperial IPA',
+    price: '8'
+  },
+  {
+    brand: 'Hopworks 2012',
+    name: 'Noggin Floggin',
+    type: 'American Barley Wine',
+    price: '6'
+  },
+  {
+    brand: 'Fremont',
+    name: 'Winter Ale',
+    type: 'Imperial Winter Warmer-Hops',
+    price: '6'
+  }
 ];
 
 function TapList() {
