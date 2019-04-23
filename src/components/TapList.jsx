e7c12f59d2c432e7186b519c133e48b602484f66
Maly Phongsavanh <phongsavanh619@icloud.com>
2019-04-22 18:01:08 -0700

    add an array of multiple taps

--- src/components/TapList.jsx
+++ src/components/TapList.jsx
@@ -1,15 +1,50 @@
 import React from 'react';
 import Tap from './Tap';
 
+var masterTapList = [
+  {
+    brand: 'WildCraft',
+    name: 'Rome Beauty',
+    type: 'Cider',
+    price: '7'
+  },
+  {
+    brand: 'Swift',
+    name: 'Swift Heirloom Batch 1: Sauvie Island Keeved Cider',
+    type: 'Dry Cider',
+    price: '12'
+  },
+  {
+    brand: 'Machine House',
+    name: 'Bramling Cross',
+    type: 'IPA',
+    price: '5'
+  },
+  {
+    brand: 'pFriem',
+    name: 'Oyster Stout',
+    type: 'Stout',
+    price: '7'
+  },
+  {
+    brand: 'Founders',
+    name: 'KBS',
+    type: 'Imperial Stout',
+    price: '11'
+  },
+  {
+    brand: 'Culmination',
+    name: 'Ol\' Money',
+    type: 'English Barley Wine',
+    price: '6'
+  },
+
+];
+
 function TapList() {
   return (
     <div>
-      <Tap
-        brand="Brand"
-        name="Cold Winter"
-        type="Porter"
-        price="9"
-      />
+
     </div>
   );
 }
