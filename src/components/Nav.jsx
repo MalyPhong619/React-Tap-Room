deaa7b52afb677b37746d626cbee9ba873070601
Maly Phongsavanh <Phongsavanh619@icloud.com>
2019-04-26 09:12:55 -0700

    style Nav a tags with color, text properties and hover

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,12 +3,24 @@ import React from 'react';
 function Nav() {
   return(
     <div>
-      <nav>
-        <a href='/'>Home</a>
-        <a href='/taplist'>Taps</a>
-      </nav>
-    </div>
-  );
-}
+      <style jsx>{`
+          a {
+            margin: 10px;
+            text-decoration: none;
+            color: black;
+            text-transform: uppercase;
+          }
+          a:hover {
+            color: red;
+          }
+          `}
+        </style>
+        <nav>
+          <a href='/'>Home</a>
+          <a href='/taplist'>Taps</a>
+        </nav>
+      </div>
+    );
+  }
 
-export default Nav;
+  export default Nav;
