18eaf67e1d7bc573be3170a343e2b252bc3b7e54
Maly Phongsavanh <Phongsavanh619@icloud.com>
2019-04-26 15:33:12 -0700

    display different components with onClick functions

--- src/components/NewKegControl.jsx
+++ src/components/NewKegControl.jsx
@@ -7,19 +7,27 @@ class NewKegControl extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      newKegForm: false
+      newKegForm: false,
+      editKeg: false
     };
-    this.addNewKeg = this.addNewKeg.bind(this)
+    this.addNewKeg = this.addNewKeg.bind(this);
+    this.editKegForm = this.editKegForm.bind(this);
   }
 
   addNewKeg(){
-    this.setState({newKegForm: true});
+    this.setState({newKegForm: true, editKef: false});
+  }
+
+  editKegForm(){
+    this.setState({editKeg: true, newKegForm: false});
   }
 
   render() {
     let visibleComponent = null;
     if (this.state.newKegForm === true) {
       visibleComponent = <NewKegTicket/> ;
+    } else if (this.state.editKeg === true) {
+      visibleComponent = <EditKeg/>;
     }
 
     return(
@@ -32,6 +40,8 @@ class NewKegControl extends React.Component {
           `}
         </style>
         <button onClick={this.addNewKeg}>Add New Keg</button>
+        <button onClick={this.editKegForm}>Edit Keg</button>
+
         {visibleComponent}
       </div>
     );
