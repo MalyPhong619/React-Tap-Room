25167adcbfb60f221a4589ccd2a93e62fcd50e9b
Maly Phongsavanh <Phongsavanh619@icloud.com>
2019-04-26 12:31:45 -0700

    change Error404 component h3 text

--- src/components/Error404.jsx
+++ src/components/Error404.jsx
@@ -3,11 +3,12 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 function Error404(props) {
+  console.log(props)
   return(
     <div>
       <div>
-        <h2>The page {props.location.pathname} does not exist!</h2>
-        <h3>Would you like to return <Link to="/">home</Link> instead?</h3>
+        <h2>The path {props.location.pathname} does not exist!</h2>
+        <h3>Navigate to <Link to="/">home</Link> page.</h3>
       </div>
     </div>
   );
