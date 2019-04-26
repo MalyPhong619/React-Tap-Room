9391085899badc74ff307a33a0c108293971f431
Maly Phongsavanh <Phongsavanh619@icloud.com>
2019-04-26 11:00:46 -0700

    try to style Link elements - unable to style

--- src/components/AdminNav.jsx
+++ src/components/AdminNav.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function AdminNav() {
   return(
     <div>
-      <a href='/'>Home</a>
-      <a href='#'>Add New Keg</a>
-      <a href='#'>Edit Existing Keg</a>
+      <style jsx>{`
+        .link {
+          margin: 10px;
+          text-decoration: none;
+          color: black;
+          text-transform: uppercase;
+          border-right: 1px dotted red;
+        }
+        .link:hover {
+          color: red;
+        }
+        `}
+      </style>
+      <Link className='link' to='/'>Home</Link> |
+      <Link className='link' to='/admin'>Add New Keg</Link> |
+      <Link className='link' to='/'>Edit Existing Keg</Link>
     </div>
   );
 }
