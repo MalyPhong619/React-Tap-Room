12b3c2529d668ac2e7a50f63745dd8cbd8dabdb5
Maly Phongsavanh <Phongsavanh619@icloud.com>
2019-04-26 10:03:40 -0700

    create input fields and submit button for admin to add new keg

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -3,7 +3,29 @@ import React from 'react';
 function Admin() {
   return(
     <div>
-      <p>Admin</p>
+      <form>
+        <input
+          type='text'
+          id='brand'
+          placeholder='Company Name'>
+        </input>
+        <input
+          type='text'
+          id='name'
+          placeholder='Keg Name'>
+        </input>
+        <input
+          type='text'
+          id='type'
+          placeholder='Beer Type'>
+        </input>
+        <input
+          type='text'
+          id='cost'
+          placeholder='Cost per Pint'>
+        </input>
+        <button type='submit'>Add Keg</button>
+      </form>
     </div>
   );
 }
