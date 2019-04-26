a79645bea975a053c18cf135b9d254154bfd1d29
Maly Phongsavanh <Phongsavanh619@icloud.com>
2019-04-26 08:43:03 -0700

    create, export, import and display Header component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import TapList from './TapList';
 import { Switch, Route } from 'react-router-dom';
+import Header from './Header';
+
 
 function App(){
   return (
     <div>
+      <Header/>
       <Switch>
         <Route exact path='/' component={TapList} />
       </Switch>
