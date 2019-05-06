c9d8411b04e8c19d7fd012746dbe744b154e7e70
Maly Phongsavanh <m.phongsavanh619@gmail.com>
2019-05-05 23:14:47 -0700

    remove AdminNav

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import AdminNav from './AdminNav';
 import NewKegControl from './NewKegControl';
 
 function Admin() {
   return(
     <div>
-      <AdminNav/>
       <NewKegControl/>
     </div>
   );
